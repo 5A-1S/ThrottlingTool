eec14f77dfd7ecebc10f6ee3e1e16ada053ce9d5
diogoremiao <diogoremiao@gmail.com>
2021-12-30 16:31:16 +0000

    time alibs

--- API/Delay.py
+++ API/Delay.py
@@ -23,12 +23,10 @@ def ping_delay_port(destination, port):
 	string = "time nc -zw30 " + destination + " " + str(port)
 	ping = subprocess.Popen(string, shell = True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-	out, error = ping.communicate()
+	out, error = ping.communicate(cd )
 	output_text = error.decode()
 	matcher = re.compile("\d+:\d+\d+.\d+\d+")
 	result = (matcher.search(output_text))
 	result = result[0].translate((str.maketrans('', '', ':')))
-	print(result)
+	#print(result)
 	return result
-
-ping_port("172.22.0.3", 80)
